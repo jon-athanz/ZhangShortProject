247b5292a0981dc25fa1f8ebd320f5472aaec84d
Jonathan Zhang <jonathanz28@nycstudents.net>
2023-05-23 09:30:18 -0400

    ZhangShortProject

--- src/MainGUIWindow.java
+++ src/MainGUIWindow.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class MainGUIWindow {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainGUIWindow extends JFrame implements ActionListener, KeyListener {
+    private JPanel mainPanel;
+    private JTextField noteTaking101TextField;
+    private JTextField textField1;
+    private JButton searchButton;
+    private JList list1;
+
+    public MainGUIWindow() {
+        createUIComponents();
+    }
+
+    private void createUIComponents() {
+        setContentPane(mainPanel);
+        setTitle("Note Taking");
+        setSize(300, 400);
+        setLocation(450, 100);
+        searchButton.addActionListener(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        list1.getItems()
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
